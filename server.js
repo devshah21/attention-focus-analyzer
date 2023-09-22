66c86238403f425e3b77cb297f660eaf5cbb3bed
devshah21 <mail2devshah@gmail.com>
2023-09-21 20:11:32 -0400

    i broke it oops

--- server.js
+++ server.js
@@ -62,6 +62,12 @@ app.post('/logout', (req, res) => {
     }
 });
 
+app.get('/', (req, res) => {
+    res.send('Hello, World!');
+});
+
+
+
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
