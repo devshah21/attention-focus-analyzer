10a381bdcb898856e0cb40528f1362ab64e0c94e
devshah21 <mail2devshah@gmail.com>
2024-01-12 12:43:59 -0500

    sampled image every couple seconds and sends model

--- app.py
+++ app.py
@@ -1,8 +1,32 @@
 from flask import Flask, render_template, Response
 import cv2
 import tensorflow as tf
+from classifier import *
+import time
 
 model = tf.keras.models.load_model('eye_Model.h5')
+array = []
+
+
+def classify_frame(frame, model):
+    # Convert the frame to a numpy array and normalize it
+    frame = cv2.resize(frame, (128, 128))
+    frame = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
+    frame = np.expand_dims(frame, axis=0)
+    frame = frame / 255.
+
+    # Use the model to make a prediction
+    prediction = model.predict(frame)
+
+    # Get the class with the highest probability
+    predicted_class = np.argmax(prediction)
+
+    if predicted_class == 0:
+        prediction = 'closed eyes'
+    else:
+        prediction = 'open eyes'
+
+    return prediction
 
 app = Flask(__name__)
 
@@ -21,10 +45,13 @@ def gen():
             break
         else:
             frame = cv2.flip(frame, 1)
+            prediction = classify_frame(frame, model)
+            array.append(prediction)
             ret, buffer = cv2.imencode('.jpg', frame)
             frame = buffer.tobytes()
             yield (b'--frame\r\n'
                    b'Content-Type: image/jpeg\r\n\r\n' + frame + b'\r\n')  # concat frame one by one and show result
+            time.sleep(2)
 
 @app.route('/video_feed')
 def video_feed():
