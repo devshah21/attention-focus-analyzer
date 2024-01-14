b6f35fa65a823fcc2134792aedf5ea06b4101733
devshah21 <mail2devshah@gmail.com>
2024-01-14 17:24:45 -0500

    created a function for the eye classification

--- app.py
+++ app.py
@@ -13,6 +13,30 @@ array = deque(maxlen=N)
 
 model = tf.keras.models.load_model('eye_Model.h5')
 
+face_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_frontalface_default.xml')
+eye_cascade = cv2.CascadeClassifier(cv2.data.haarcascades + 'haarcascade_eye_tree_eyeglasses.xml')
+
+
+
+
+
+def eye_classifer(img):
+    gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+    faces = face_cascade.detectMultiScale(gray, 1.3, 4)
+    
+    for (x,y,w,h) in faces:
+        roi_gray = gray[y:y+h, x:x+w]
+        roi_color = img[y:y+h, x:x+w]
+        
+        # detects eyes of within the detected face area (roi)
+        eyes = eye_cascade.detectMultiScale(roi_gray)
+        
+        # draw a rectangle around eyes
+        for (ex,ey,ew,eh) in eyes:
+            cv2.rectangle(roi_color,(ex,ey),(ex+ew,ey+eh),(0,255,255),2)
+    
+    
+
 
 def is_focused():
     # Calculate the proportion of 'closed eyes' predictions in the last N seconds
